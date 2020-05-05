ae895c81e87d4eca63dbc65939d80c3b4b1bafa2
CYBER KAD <64524950+CYBER-KAD@users.noreply.github.com>
2020-05-05 18:55:39 +0100

    main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-
+function load() {
+  alert("fully responsive app ui maybe I will build the app later.. But for now it is only UI $ UX DESIGNS") 
+  alert("Just playing around with bootstraps 4 and also kidding with grid system all together with Carousel and Flex system ") ;
+  
+} 
 function about(){
     $(".about").slideToggle()
 }
