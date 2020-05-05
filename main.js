3d738b26d4ba46ec06abd4258bf63de6dff417b2
CYBER KAD <64524950+CYBER-KAD@users.noreply.github.com>
2020-05-05 18:32:57 +0100

    main.js

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-alert(" NOTICE!!!
-        JUST PLAYING AROUND...  
-        PLAYING WITH BOOTSTRAP !!!
-also exploring more on my new belove grid system flex-box......
-working with me new friend  "carousel in bootstrap 4"
-
-")
-
+window.onload=function() {alert("NOTICE!!!.. JUST PLAYING AROUND...PLAYING WITH BOOTSTRAP !!!also exploring more on my new belove grid system flex-box......
+working with me new friend  carousel in bootstrap 4")
+} 
 function about(){
     $(".about").slideToggle()
 }
